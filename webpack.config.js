f38f69155e6cb74f3e4e3c0ad19dbb258ab2fec5
berend.campen@stud.hs-emden-leer.de <WZBs!8R})hdH5 =~T!Tce}oS#pu{m Gqi_=.e&%+=B]Du^)l7i* (tD$s4!}kpo>
2017-10-23 18:35:14 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+  entry: './scripts/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
