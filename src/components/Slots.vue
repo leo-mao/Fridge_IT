4989bd330c2facce29679edb8889b7cdb4011ae4
leo-mao <emicool.my@gmail.com>
2017-11-21 08:58:17 +0100

    color and text insert for slots

--- src/components/Slots.vue
+++ src/components/Slots.vue
@@ -24,7 +24,23 @@
   /* eslint-disable linebreak-style */
   import Hero from './fragments/Hero';
 
-  function drawCanvas(canvas, temp, filled, size) {
+  function drawCanvas(canvas, temp, filled, size, text) {
+//    const colorPrimary = '#3d8dc6';
+//    const colorPrimaryDark = '#2f6d99';
+//    const colorAccent = '#ff7040';
+    const colorText = '#0e0e0e';
+//    const colorDisabled = '#a6a6a6';
+    const colorEmpty = 'b9b9b9';
+    const colorEmptyBorder = '#878787';
+    const colorVeryWarm = '#fd4d79';
+    const colorVeryWarmBorder = '#c42c52';
+//    const colorMediumWarm = '#ff976a';
+//    const colorMediumWarmBorder = '#f17532';
+//    const colorMediumCold = '#6fc4ff';
+//    const colorMediumColdBorder = '#4699d4';
+    const colorVeryCold = '#3d8dc6';
+    const colorVeryColdBorder = '#2f6d99';
+
     const length = size;
     const lineWidth = 0.1 * length;
     const buffer = 0;
@@ -37,35 +53,37 @@
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.lineWidth = lineWidth;
     if (!filled) {
-      ctx.strokeStyle = '#868686';
-      ctx.fillStyle = '#8E8E8E';
+      ctx.strokeStyle = colorEmptyBorder;
+      ctx.fillStyle = colorEmpty;
     } else if (temp > 4) {
-      ctx.strokeStyle = '#C4584F';
-      ctx.fillStyle = '#FF6F6F';
+      ctx.strokeStyle = colorVeryWarmBorder;
+      ctx.fillStyle = colorVeryWarm;
     } else {
-      ctx.strokeStyle = '#316D96';
-      ctx.fillStyle = '#6FC3FF';
+      ctx.strokeStyle = colorVeryColdBorder;
+      ctx.fillStyle = colorVeryCold;
     }
     ctx.beginPath();
     // eslint-disable-next-line
     ctx.arc(length / 2, length / 2, radius, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.fill();
+    ctx.font = '30px Comic Sans MS';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = colorText;
+    ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+    // eslint-disable-next-line
+    console.log('text:', text);
   }
 
   // eslint-disable-next-line
-  function drawSlot(slotNr, temp, filled, size) {
+  function drawSlot(slotNr, temp, filled, size, text) {
     // eslint-disable-next-line
     const canvas = document.getElementById('slot-' + slotNr);
-    return drawCanvas(canvas, temp, filled, size);
+    return drawCanvas(canvas, temp, filled, size, text);
   }
 
-  function redrawSlot(slotNr, temp, filled, size) {
-    // eslint-disable-next-line
-//    const canvas = document.getElementById('slot-' + slotNr);
-//    const ctx = canvas.getContext('2d');
-//    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSlot(slotNr, temp, filled, size);
+  function redrawSlot(slotNr, temp, filled, size, text) {
+    drawSlot(slotNr, temp, filled, size, text);
   }
 
   function initialAllSlots(slotAmount, size) {
@@ -80,7 +98,7 @@
       // eslint-disable-next-line
       canvas.setAttribute('id', 'slot-' + slotID);
       href.appendChild(canvas);
-      drawCanvas(canvas, slotID % 8, true, size);
+      drawCanvas(canvas, slotID % 8, true, size, slotID);
       column.appendChild(href);
       div.appendChild(column);
     }
@@ -93,14 +111,14 @@
     data() {
       return {
         amount: 24,
-        windowWidth: window.innerWidth
+        windowWidth: window.innerWidth,
       };
     },
     methods: {
       handleWindowResize(event) {
         this.windowWidth = event.currentTarget.innerWidth;
-        for (let slot = 0; slot < this.amount; slot += 1) {
-          redrawSlot(slot, slot % 8, true, this.windowWidth / 5);
+        for (let slotID = 0; slotID < this.amount; slotID += 1) {
+          redrawSlot(slotID, slotID % 8, true, this.windowWidth / 5, slotID);
         }
       },
     },
