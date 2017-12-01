4c0dd4f1b0729c621fcc31a5c27b77ef8c1fa19f
BerryPlay <campen.berend@gmail.com>
2017-12-01 11:14:53 +0100

    finished slot (with data from server)

--- src/components/fragments/Linechart.vue
+++ src/components/fragments/Linechart.vue
@@ -1,12 +1,51 @@
 <script>
-  // LineChart.js
-  import { Line } from 'vue-chartjs';
+  import { Line, mixins } from 'vue-chartjs';
+
+  const { reactiveProp } = mixins;
 
   export default {
     extends: Line,
-    props: ['data', 'options'],
+    mixins: [reactiveProp],
     mounted() {
-      this.renderChart(this.data, this.options);
+      // this.chartData is created in the mixin.
+      // If you want to pass options please create a local options object
+      this.renderChart(this.chartData, {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false,
+            },
+          }],
+          xAxes: [{
+            display: true,
+            labelString: 'probability',
+          }],
+        },
+      });
     },
   };
+
+  /*
+  // LineChart.js
+  import { Line, mixins } from 'vue-chartjs';
+  
+  const { reactiveProp } = mixins;
+
+  export default {
+    extends: Line,
+    props: ['data'],
+    mixins: [reactiveProp],
+    mounted() {
+      this.renderChart(this.data, {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false,
+              },
+            }],
+          },
+      });
+    },
+  };
+  */
 </script>
